9c336a95b00b394af5fa2ec649c421a52178e77c
Stevinel <stevinel@xaker.ru>
2021-09-29 09:11:33 +0300

    no input2

---
+++ unloved_business/unloved business.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, value, next_item=None):
+        self.value = value
+        self.next_item = next_item
+
+    def __str__(self):
+        return self.value
+
+def get_node_by_index(node, index):
+    while index:
+        node = node.next_item
+        index -= 1
+    return node
+
+
+def solution(node, idx):
+    if idx == 0:
+        return node.next_item
+    print(node)
+    previous_node = get_node_by_index(node, idx-1)
+    next_node = get_node_by_index(node, idx+1)
+    previous_node.next_item = next_node
+    return node
+
