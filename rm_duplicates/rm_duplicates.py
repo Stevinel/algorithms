54f9dc01fbfec0b9b796003857111b13cda8f15d
Stevinel <stevinel@xaker.ru>
2022-08-17 16:42:12 +0300

    Remove Duplicates from Sorted Array

---
+++ rm_duplicates/rm_duplicates.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    """Runtime: 241ms"""
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 1
+        while i < len(nums):
+            if nums[i] == nums[i - 1]:
+                nums.pop(i)
+            else:
+                i += 1
+        return len(nums)
+
+
+# class Solution:
+#     """Runtime: 163ms"""
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         nums[:] = sorted(set(nums))
+#         return len(nums)
+
+
+if __name__ == '__main__':
+    nums = list(map(int, input().split()))
+    s = Solution()
+    print(s.removeDuplicates(nums))
