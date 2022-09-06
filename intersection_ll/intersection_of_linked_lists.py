db33f3f49c984bf5b39c4cf725d4ab2f4d7ae5e2
Stevinel <stevinel@xaker.ru>
2022-09-06 14:57:08 +0300

    intersection

---
+++ intersection_ll/intersection_of_linked_lists.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    """Runtime: 320ms"""
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        node_set = set()
+
+        while headA: 
+            node_set.add(headA)
+            headA = headA.next
+            
+        while headB:
+            if headB in node_set: 
+                return headB
+            headB = headB.next
